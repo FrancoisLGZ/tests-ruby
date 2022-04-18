d24a1132c91f423054b267d878ce21a593d6ac4d
FrancoisLGZ <francis.legouez@gmail.com>
2022-04-18 21:57:38 +0200

    simon complété + commenté

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -2,14 +2,22 @@ def echo(a)
   return a
 end
 
-def echo(b)
-  return b
+def shout(b)
+  return b.upcase
 end
 
-def shout(c)
-  return c.upcase
+def repeat(c,x)
+  return (c * x) #Manque l'espace et est fixé à x (fonctionne donc pour l'exemple avec 3). Comment imprimer 2 hello sans mettre de x comme dans le test ??
 end
 
-def repeat(d)
-  return 2.times d
+def start_of_word(d,y)
+  return d[0,y] #Formule sur internet + test à tâton.
+end
+
+def first_word(e)
+  e.split.first #Internet
+end
+
+def titleize(f) #Compliqué + manque de temps
+
 end
